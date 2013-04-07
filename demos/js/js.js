d24a4277415102a16b7fcaab5c6a654dc78150bf
webinista <tiffany@webinista.com>
2013-04-07 12:31:31 -0700

    Adding demos for matchMedia, and an example of using media queries with SVG.

--- demos/js/js.js
+++ demos/js/js.js
@@ -17,4 +17,21 @@ Utilities.prototype.stripNWChars = function(inputText){
 	return inputText.replace(/\W/gi,'');
 }
 
+Utilities.prototype.whichVideoType = function(){
+	var vid = document.querySelector('video') || document.createElement('video');
+	var types = {
+	  'mp4' :'video/mp4; codecs="avc1.42E01E, mp4a.40.2"', 
+	  'webm':'video/webm; codecs="vp8, vorbis"',
+	  'ogv' :'video/ogg; codecs="theora, vorbis"'
+	}
+	
+	for(t in types){
+		var cpt = vid.canPlayType( types[t] );
+		if( cpt !== ''){
+			return t; // return the first maybe or probably
+			break;    // break after that.
+		}	
+	}
+}
+
 var Util = new Utilities();
